8c4eda4eab3cece43768b177d5f18699ce9239e2
DryburgB <dryburgb@sheridancollege.ca>
2023-05-26 13:34:47 -0400

    no message

--- ToJam 2023/Assets/Scripts/PlayerMovement.cs
+++ ToJam 2023/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] [Tooltip("Amount of time it takes for player to move from one tile to the next")] private float moveSpeed;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    [SerializeField] private LayerMask colliderLayer;
+>>>>>>> origin/main
 =======
     [SerializeField] private LayerMask colliderLayer;
 >>>>>>> origin/main
@@ -28,24 +32,36 @@ public class PlayerMovement : MonoBehaviour
             if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
             {
 <<<<<<< HEAD
+<<<<<<< HEAD
                 movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
 =======
+=======
+>>>>>>> origin/main
                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0), 0.2f, colliderLayer))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
                 }
+<<<<<<< HEAD
+>>>>>>> origin/main
+=======
 >>>>>>> origin/main
             }
 
             if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
             {
 <<<<<<< HEAD
+<<<<<<< HEAD
                 movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
 =======
+=======
+>>>>>>> origin/main
                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, Input.GetAxisRaw("Vertical"), 0), 0.2f, colliderLayer))
                 {
                     movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);   
                 }
+<<<<<<< HEAD
+>>>>>>> origin/main
+=======
 >>>>>>> origin/main
             }
         }
