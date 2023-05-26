2b1582c350b8d37b65080820fa79770a309d429b
DryburgB <dryburgb@sheridancollege.ca>
2023-05-26 13:52:42 -0400

    implimented visibility into the player

---
+++ ToJam 2023/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] [Tooltip("Amount of time it takes for player to move from one tile to the next")] private float moveSpeed;
+    [SerializeField] private LayerMask colliderLayer;
+    private Transform movePoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint = gameObject.transform.Find("Move Point");
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if(transform.position == movePoint.position)
+        {
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0), 0.2f, colliderLayer))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+                }
+            }
+
+            if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, Input.GetAxisRaw("Vertical"), 0), 0.2f, colliderLayer))
+                {
+                    movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);   
+                }
+            }
+        }
+    }
+}
