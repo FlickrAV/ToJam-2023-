cf2e8655402ad7958a5905bea77ea1a9cb550b78
DryburgB <dryburgb@sheridancollege.ca>
2023-05-27 01:51:22 -0400

    got a decent version of the limb menu working, complete with a throw mode when you have arms, and a drop mode when you don't

--- ToJam 2023/Assets/Scripts/LimbDetatchMenu.cs
+++ ToJam 2023/Assets/Scripts/LimbDetatchMenu.cs
@@ -1,21 +1,191 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class LimbDetatchMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject[] limbs;    
+    [SerializeField] private GameObject[] limbs;
+    [SerializeField] private GameObject[] menuButtons;
     [SerializeField] private Transform limbSpawnPosition;
     [SerializeField] private PlayerMovement playerScript;
+    [SerializeField] private GameObject limbDetatchMenu;
+    [SerializeField] private GameObject throwDestinationIcon;
+    [SerializeField] private GameObject playerVisionSquare;
 
+    [HideInInspector] public bool inThrowMode, inDropMode;
 
+    private bool[] hasLimb = new bool[6];
 
-    public void Detatch(GameObject limbToDetatch)
+    private bool hasRightEye, hasLeftEye, hasRightArm, hasLeftArm, hasRightLeg, hasLeftLeg = true;
+
+    private bool hasArms = true;
+    private bool hasLegs = true;
+    private bool hasEyes = true;
+
+    private int currentLimb = 0;
+
+    private void Start()
     {
-        if(playerScript.canMove && !Physics2D.OverlapCircle(limbSpawnPosition.position, 0.2f, playerScript.colliderLayer))
-        Instantiate(limbs[int.Parse(limbToDetatch.name)], limbSpawnPosition.position, limbSpawnPosition.rotation);
+        //sets both modes to false
+        inDropMode= false;
+        inThrowMode = false;
 
+        //sets all the booleans in the hasLimb array to true
+        for (int i = 0; i < limbs.Length; i++)
+        {
+            hasLimb[i] = true;
+        }
+    }
+
+    private void Update()
+    {
+        //sets every has[Insert Limb] boolean equal to their numbered counterpart
+        hasRightEye= hasLimb[0];
+        hasLeftEye= hasLimb[1];
+        hasRightArm= hasLimb[2];
+        hasLeftArm= hasLimb[3];
+        hasRightLeg= hasLimb[4];
+        hasLeftLeg= hasLimb[5];
+
+        //opens the limb menu when the E key is pressed if the menu is not already open
+        if (Input.GetKeyDown(KeyCode.E) && !limbDetatchMenu.activeSelf)
+        {
+            limbDetatchMenu.SetActive(true);
+        }
+        //closes the limb meny when the E key is pressed if the menu is already open
+        else if (Input.GetKeyDown(KeyCode.E) && limbDetatchMenu.activeSelf)
+        {
+            limbDetatchMenu.SetActive(false);
+        }
+
+
+        if (inThrowMode)
+        {
+            //closes the limb menu and turns on the limb destination indicator
+            limbDetatchMenu.SetActive(false);
+            throwDestinationIcon.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                /*
+                if the left mouse button is pressed, 
+                spawns the limb at the location 
+                of the limb destination indicator,
+                turns off the corrisponding button
+                in the menu, turns off the hasLimb
+                boolean and exits throw mode
+                */
+                    Instantiate(limbs[currentLimb], throwDestinationIcon.transform.position, limbSpawnPosition.rotation);
+                    menuButtons[currentLimb].SetActive(false);
+                    hasLimb[currentLimb] = false;
+                    inThrowMode = false;
+            }
+
+            //exits throw mode if right click, E or Escape are pressed
+            if (Input.GetMouseButtonDown(1))
+            {
+                inThrowMode = false;
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                inThrowMode = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                inThrowMode = false;
+            }
+
+
+        }
+        else if (inDropMode)
+        {
+            //closes the limb menu and turns on the limb destination indicator
+            limbDetatchMenu.SetActive(false);
+            throwDestinationIcon.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                /*
+               if the left mouse button is pressed, 
+               spawns the limb at the location 
+               of the limb destination indicator,
+               turns off the corrisponding button
+               in the menu, turns off the hasLimb
+               boolean and exits drop mode
+               */
+                    Instantiate(limbs[currentLimb], throwDestinationIcon.transform.position, limbSpawnPosition.rotation);
+                    menuButtons[currentLimb].SetActive(false);
+                    hasLimb[currentLimb] = false;
+                    inDropMode = false;
+            }
+
+            //exits throw mode if right click, E or Escape are pressed
+            if (Input.GetMouseButtonDown(1))
+            {
+                inDropMode = false;
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                inDropMode = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                inDropMode = false;
+            }
+        }
+        //turns off the limb destination indicator when you are no longer in throw or drop mode
+        else
+        {
+            throwDestinationIcon.SetActive(false);
+        }
+
+
+
+        //controls the booleans that say whether or not you have both of a certain type of limb
+        if (!hasLeftArm && !hasRightArm)
+        {
+            hasArms= false;
+        }
+        if (!hasRightLeg && !hasLeftLeg)
+        {
+            hasLegs= false;
+        }
+        if (!hasLeftEye && !hasRightEye)
+        {
+            hasEyes= false;
+        }
+
+        //turns off the vision square if the player doesn't have any eyes
+        if (!hasEyes)
+        {
+            playerVisionSquare.SetActive(false);
+        }
+    }
+
+
+    public void ButtonPressed(GameObject limbToDetatch)
+    {
+        //sets the currentLimb variable equal to limbToDetatch for the rest of the script to use
+        currentLimb = int.Parse(limbToDetatch.name);
+
+        if (hasArms)
+        {
+            //enable throw mode if the player has any arms left when a button is selected in the limb menu
+            inThrowMode = true;
+
+            //turns off the limb menu
+            limbDetatchMenu.SetActive(false);
+        }
+        else if (!hasArms)
+        {
+            //enable drop mode if the player has no arms left when a button is selected in the limb menu
+            inDropMode= true;
+
+            //turns off the limb menu
+            limbDetatchMenu.SetActive(false);
+        }
     }
 }
--- ToJam 2023/Assets/Scripts/ThrowLocationAllignment.cs
+++ ToJam 2023/Assets/Scripts/ThrowLocationAllignment.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class ThrowLocationAllignment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public LimbDetatchMenu limbManagerScript;
 
-    // Update is called once per frame
     void Update()
     {
+        //converts the location of the mouse to a location in the world
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.position = new Vector3(Mathf.Floor(mousePosition.x) + 0.5f, Mathf.Floor(mousePosition.y) + 0.5f, transform.position.z);
+
+        if (limbManagerScript.inThrowMode)
+        {
+            //moves it's position to the position of the mouse, snapping to the middle of the tile
+            transform.position = new Vector3(Mathf.Floor(mousePosition.x) + 0.5f, Mathf.Floor(mousePosition.y) + 0.5f, transform.position.z);
+        }
+        else if (limbManagerScript.inDropMode)
+        {
+            //does some math to find the angle between the player and the mouse in order to find where to place it
+            Vector2 playerPosition = player.transform.position;
+            float angle = Mathf.Atan2(mousePosition.y - playerPosition.y, mousePosition.x - playerPosition.x);
+
+            //moves it's position to one tile away from the player in the direction of the mouse, snapping to the middle of the tile
+            transform.position = new Vector2(Mathf.Floor(playerPosition.x + Mathf.Sin(-angle + Mathf.Deg2Rad * 90)) + 0.5f, Mathf.Floor(playerPosition.y + Mathf.Cos(-angle + Mathf.Deg2Rad * 90)) + 0.5f);
+        }
     }
 }
