2f5dec914904128e9298f2402cd3f59e31784493
DryburgB <dryburgb@sheridancollege.ca>
2023-05-27 18:54:36 -0400

    restricted the throwing of limbs to only be within the view square and anything that isn't a collision

--- ToJam 2023/Assets/Scripts/ThrowLocationAllignment.cs
+++ ToJam 2023/Assets/Scripts/ThrowLocationAllignment.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class ThrowLocationAllignment : MonoBehaviour
 {
     public GameObject player;
-    public LimbDetatchMenu limbManagerScript;
+    public Sprite canThrowIcon, cantThrowIcon;
+    [HideInInspector]public LimbDetatchMenu limbManagerScript;
+    [HideInInspector]public bool canThrowLimb = false;
+    private bool onACollider = false;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        limbManagerScript = GameObject.Find("Limb Manager").gameObject.GetComponent<LimbDetatchMenu>();
+
+        spriteRenderer= gameObject.GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
+        if (canThrowLimb)
+        {
+            spriteRenderer.sprite = canThrowIcon;
+        }
+        else if (!canThrowLimb)
+        {
+            spriteRenderer.sprite = cantThrowIcon;
+        }
+
+
         //converts the location of the mouse to a location in the world
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -26,6 +48,52 @@ public class ThrowLocationAllignment : MonoBehaviour
 
             //moves it's position to one tile away from the player in the direction of the mouse, snapping to the middle of the tile
             transform.position = new Vector2(Mathf.Floor(playerPosition.x + Mathf.Sin(-angle + Mathf.Deg2Rad * 90)) + 0.5f, Mathf.Floor(playerPosition.y + Mathf.Cos(-angle + Mathf.Deg2Rad * 90)) + 0.5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Vision Square"))
+        {
+            if (!onACollider)
+            {
+                canThrowLimb = true;
+            }
+        }
+        if (collision.gameObject.CompareTag("Collisions"))
+        {
+            onACollider = true;
+            canThrowLimb = false;
+        }            
+
+    }    
+    
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Vision Square"))
+        {
+            if (!onACollider)
+            {
+                canThrowLimb = true;
+            }
+        }
+        if (collision.gameObject.CompareTag("Collisions"))
+        {
+            onACollider = true;
+            canThrowLimb = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Vision Square"))
+        {
+            canThrowLimb = false;
+        }
+        if (collision.gameObject.CompareTag("Collisions"))
+        {
+            onACollider = false;
         }
     }
 }
