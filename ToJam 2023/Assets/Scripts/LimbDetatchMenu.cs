c3b56a540aad64f647bd4356485ecdee4a3a83ae
EvaBlinova <127451157+EvaBlinova@users.noreply.github.com>
2023-05-26 20:18:02 -0400

    Commit
    First commit

---
+++ ToJam 2023/Assets/Scripts/LimbDetatchMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbDetatchMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] limbs;    
+    [SerializeField] private Transform limbSpawnPosition;
+    [SerializeField] private PlayerMovement playerScript;
+
+    public void Detatch(GameObject limbToDetatch)
+    {
+        if(playerScript.canMove && !Physics2D.OverlapCircle(limbSpawnPosition.position, 0.2f, playerScript.colliderLayer))
+        Instantiate(limbs[int.Parse(limbToDetatch.name)], limbSpawnPosition.position, limbSpawnPosition.rotation);
+    }
+}
