de5c6973cf7f33eac2dfb9ec988bca013927878f
Axel Vera Jimenez <63276968+FlickrAV@users.noreply.github.com>
2023-05-27 15:05:03 -0400

    limb interactions kinda sorta mayyyyybe work

--- ToJam 2023/Assets/Scripts/Arm.cs
+++ ToJam 2023/Assets/Scripts/Arm.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Arm : MonoBehaviour
 {
-    public List<GameObject> nearbyInteractables;
+    private List<Interactable> interactables = new List<Interactable>();
+    private bool isInteracting = false;
+    [HideInInspector] public bool isUsed = false;
 
-    private void OnMouseDown() 
+
+    private void Update()
     {
-        foreach(GameObject interactable in nearbyInteractables)
+        if (Input.GetButtonDown("Cancel") && isInteracting)
         {
-            if(interactable.GetComponent<Interactable>() != null)
+            foreach (Interactable interactable in interactables)
             {
-                interactable.GetComponent<Interactable>().canClick = true;
+                interactable.limbCanInteract = false;
+                interactable.gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+         if (interactables.Capacity > 0)
+        {
+            isInteracting = true;
+            foreach (Interactable interactable in interactables)
+            {
+                interactable.limbCanInteract = interactable.InteractionCheck(this.gameObject);
+                interactable.limbScript = this;
+                interactable.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Interactable")
+        {
+            interactables.Add(other.gameObject.GetComponent<Interactable>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Interactable")
+        {
+            foreach (Interactable script in interactables)
+            {
+                if (script == other.GetComponent<Interactable>())
+                {
+                    interactables.Remove(script);
+                    interactables.TrimExcess();
+                }
             }
         }
     }
