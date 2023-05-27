eecc14379d9f5ea58a4754612a0a454ff40ee371
dexc34 <dexc43@gmail.com>
2023-05-27 16:42:04 -0400

    Button Rework and Exit Rework
    -The exit can now close after being depressed
    -Buttons can now be depressed if the thing activating it leaves its radius

--- ToJam 2023/Assets/Scripts/Button.cs
+++ ToJam 2023/Assets/Scripts/Button.cs
@@ -6,21 +6,46 @@ public class Button : MonoBehaviour
 {
     [SerializeField] private GameObject[] objectsToCall;
     [SerializeField] private string[] functionsToCall;
+
+    [SerializeField] private GameObject[] objectsToCallOnDepress;
+    [SerializeField] private string[] functionsToCallOnDepress;
+
+    private bool isPressed = false;
+
     private Interactable interactableScript;
 
     private void Start() 
     {
         interactableScript = GetComponent<Interactable>();
+        Debug.Log(interactableScript.InRange());
     }
 
+    private void Update()
+    {
+        Debug.Log(interactableScript.thingsInRange);
+        if (interactableScript.thingsInRange == 0 && isPressed)
+        {
+            ButtonDepressed();
+        }
+    }
 
     public void ButtonPressed()
     {
-        for(int i = 0; i < objectsToCall.Length;i++)
+        for (int i = 0; i < objectsToCall.Length; i++)
         {
             objectsToCall[i].SendMessage(functionsToCall[i]);
+            isPressed = true;
         }
-    }   
+    }
+
+    public void ButtonDepressed()
+    {
+        for (int i = 0; i < objectsToCallOnDepress.Length; i++)
+        {
+            objectsToCallOnDepress[i].SendMessage(functionsToCallOnDepress[i]);
+            isPressed = false;
+        }
+    }
 
     private void OnMouseDown() 
     {
