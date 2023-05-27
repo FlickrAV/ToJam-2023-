2171c9380c07c8477104911eaaba7e85c151a6c3
MaacReat <127222403+MaacReat@users.noreply.github.com>
2023-05-27 17:56:41 -0400

    a

--- ToJam 2023/Assets/Scripts/Button.cs
+++ ToJam 2023/Assets/Scripts/Button.cs
@@ -6,28 +6,55 @@ public class Button : MonoBehaviour
 {
     [SerializeField] private GameObject[] objectsToCall;
     [SerializeField] private string[] functionsToCall;
+
+    [SerializeField] private GameObject[] objectsToCallOnDepress;
+    [SerializeField] private string[] functionsToCallOnDepress;
+
+    private bool isPressed = false;
+
     private Interactable interactableScript;
 
     private void Start() 
     {
         interactableScript = GetComponent<Interactable>();
+        Debug.Log(interactableScript.InRange());
     }
 
+    private void Update()
+    {
+        Debug.Log(interactableScript.thingsInRange);
+        if (interactableScript.thingsInRange == 0 && isPressed)
+        {
+            ButtonDepressed();
+        }
+    }
 
     public void ButtonPressed()
     {
-        for(int i = 0; i < objectsToCall.Length;i++)
+        for (int i = 0; i < objectsToCall.Length; i++)
         {
             objectsToCall[i].SendMessage(functionsToCall[i]);
+            isPressed = true;
+            Debug.Log(isPressed);
         }
-    }   
+    }
+
+    public void ButtonDepressed()
+    {
+        for (int i = 0; i < objectsToCallOnDepress.Length; i++)
+        {
+            objectsToCallOnDepress[i].SendMessage(functionsToCallOnDepress[i]);
+            isPressed = false;
+        }
+    }
 
     private void OnMouseDown() 
     {
-        if(interactableScript.InRange())    
+        if(interactableScript.InRange() && !isPressed)    
         {
             if(interactableScript.limbCanInteract)
             {
+                Debug.Log("if you see this you've done something wrong (button code)");
                 interactableScript.limbsUsed += 1;
                 if(interactableScript.limbsUsed == interactableScript.arms || interactableScript.limbsUsed == interactableScript.legs)
                 {
@@ -38,7 +65,6 @@ public class Button : MonoBehaviour
             }
             else if(interactableScript.PlayerInteractionCheck())
             {
-                Debug.Log("wah");
                 ButtonPressed();
             }
         }
