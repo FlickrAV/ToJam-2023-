03ae7ff96b7e5ce07ec8da85e3cfd1660f079971
MaacReat <127222403+MaacReat@users.noreply.github.com>
2023-05-28 15:50:53 -0400

    Fixed Electrical Field prefab(new script)

---
+++ ToJam 2023/Assets/Scripts/EField.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EField : MonoBehaviour
+{
+    [SerializeField] private GameObject exitLock;
+
+    public bool isOpen = false;
+
+    private SpriteRenderer exitSprite;
+    public Sprite exitOpen;
+    public Sprite exitClose;
+
+    public SpriteRenderer exitSpriteDark;
+
+    private void Start()
+    {
+        exitSprite = gameObject.GetComponent<SpriteRenderer>();
+        exitSprite.sprite = exitClose;
+        if (isOpen)
+        {
+            exitSprite.sprite = exitOpen;
+            exitLock.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void DestroyLock()
+    {
+        exitSprite.sprite = exitOpen;
+        exitSpriteDark.sprite = exitOpen;
+        exitLock.SetActive(false);
+    }
+
+    public void RespawnLock()
+    {
+        exitSprite.sprite = exitClose;
+        exitSpriteDark.sprite = exitClose;
+        exitLock.SetActive(true);
+    }
+}
